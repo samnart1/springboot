b6e42700ae35c2d29c58a8dea54997db06dc9701
samnart1 <originalsamnart@gmail.com>
2025-01-27 12:10:42 +0100

    created to create custom exceptions

---
+++ ecommerce/src/main/java/com/samnart/ecommerce/exception/APIException.java
@@ -0,0 +1,12 @@
+package com.samnart.ecommerce.exception;
+
+public class APIException extends RuntimeException {
+    private static final long serialVersionUID = 1l;
+
+    public APIException() {}
+
+    public APIException(String message) {
+        super(message);
+    }
+    
+}
