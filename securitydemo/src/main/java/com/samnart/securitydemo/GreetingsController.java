0fb283c127148f00bfd759a706d0a4a856fe3c14
samnart1 <originalsamnart@gmail.com>
2025-01-31 19:40:25 +0100

    asdf

--- securitydemo/src/main/java/com/samnart/securitydemo/GreetingsController.java
+++ securitydemo/src/main/java/com/samnart/securitydemo/GreetingsController.java
@@ -1,8 +1,10 @@
 package com.samnart.securitydemo;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/api")
@@ -10,7 +12,20 @@ public class GreetingsController {
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello Bob!";
+        return "Missing my Painter's boobs!!!";
     }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String userEndpoint() {
+        return new String("Hello, User!");
+    }
+
+    @GetMapping("/admin")
+    public String adminEndpoint() {
+        return new String("Hello, Admin");
+    }
+    
+    
     
 }
