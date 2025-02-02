fe80e883f41a6e3379d57fa31f1727b5669e0715
samnart1 <originalsamnart@gmail.com>
2025-02-02 02:05:07 +0100

    auth entry point, login response and login dto

---
+++ securitydemo/src/main/java/com/samnart/securitydemo/jwt/LoginResponse.java
@@ -0,0 +1,23 @@
+package com.samnart.securitydemo.jwt;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponse {
+    private String jwtToken;
+    
+    private String username;
+    private List<String> roles;
+
+    public LoginResponse(String username, List<String> roles, String jwtToken) {
+        this.username = username;
+        this. roles = roles;
+        this.jwtToken = jwtToken;
+    }
+}
