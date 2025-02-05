0c5e84d52ac5d538bbf29f59d7ee6178bebb64a0
samnart1 <originalsamnart@gmail.com>
2025-02-05 18:09:50 +0100

    spring security

--- securitydemo/src/main/java/com/samnart/securitydemo/config/SecurityConfig.java
+++ securitydemo/src/main/java/com/samnart/securitydemo/config/SecurityConfig.java
@@ -1,53 +1,57 @@
-package com.samnart.securitydemo.config;
-
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.samnart.securitydemo.jwt.AuthEntryPointJwt;
-import com.samnart.securitydemo.jwt.AuthTokenFilter;
-
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
-    
+
     @Autowired
     private DataSource dataSource;
 
-    @Autowired
-    private AuthEntryPointJwt unauthorizedHandler;
-
     @Bean
-    public AuthTokenFilter authenticationJwtTokenFilter() {
-        return new AuthTokenFilter();
+    SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(requests -> requests
+                .requestMatchers("/h2-console/**").permitAll()
+                .requestMatchers("/api/login").permitAll()
+                .anyRequest().authenticated()
+            )
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizeHandler))
+            .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()))
+            .addFilterBefore(null, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+            
     }
 
     @Bean
-    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                .requestMatchers("/h2-console/**").permitAll()
-                .requestMatchers("/api/signin").permitAll()
-                .anyRequest().authenticated()
-            )
+    public JdbcUserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
 
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
-            .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()))
-            .addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-        
-        return http.build();
-        
-        
-        
-        
-        // http.authorizeHttpRequests((requests) -> requests.requestMatchers("/h2-console/**").permitAll())
-        // .build();
+    @Bean 
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    public AuthenticationManager authenticationManager (AuthenticationConfiguration builder) throws Exception {
+        return builder.getAuthenticationManager();
     }
 }
