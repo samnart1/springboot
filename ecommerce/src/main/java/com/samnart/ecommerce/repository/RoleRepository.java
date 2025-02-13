ed567c19357b87ab05588de51ae1173f41222d8f
samnart1 <originalsamnart@gmail.com>
2025-02-13 20:20:03 +0100

    created the role repository and worked on signup request dto and message response dto

---
+++ ecommerce/src/main/java/com/samnart/ecommerce/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.samnart.ecommerce.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.samnart.ecommerce.model.AppRole;
+import com.samnart.ecommerce.model.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByRoleName(AppRole roleUser);
+    
+}
