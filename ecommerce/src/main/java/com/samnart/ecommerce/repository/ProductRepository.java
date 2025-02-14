e3f9566097a3d6a84ba1108e4149be8a062f04ba
samnart1 <originalsamnart@gmail.com>
2025-02-14 15:37:12 +0100

    repositories ready for now, will be modified further later

--- ecommerce/src/main/java/com/samnart/ecommerce/repository/ProductRepository.java
+++ ecommerce/src/main/java/com/samnart/ecommerce/repository/ProductRepository.java
@@ -1,18 +1,38 @@
 package com.samnart.ecommerce.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.samnart.ecommerce.model.Category;
 import com.samnart.ecommerce.model.Product;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {}
 
-    Page<Product> findByCategoryOrderByPriceAsc(Category category, Pageable pageDetails);
 
-    Page<Product> findByProductNameLikeIgnoreCase(String keyword, Pageable pageDetails);
+
+
+
+
+
+
+
+
+
+// package com.samnart.ecommerce.repository;
+
+// import org.springframework.data.domain.Page;
+// import org.springframework.data.domain.Pageable;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.stereotype.Repository;
+
+// import com.samnart.ecommerce.model.Category;
+// import com.samnart.ecommerce.model.Product;
+
+// @Repository
+// public interface ProductRepository extends JpaRepository<Product, Long> {
+
+//     Page<Product> findByCategoryOrderByPriceAsc(Category category, Pageable pageDetails);
+
+//     Page<Product> findByProductNameLikeIgnoreCase(String keyword, Pageable pageDetails);
     
-}
+// }
--- ecommerce/src/main/java/com/samnart/ecommerce/repository/UserRepository.java
+++ ecommerce/src/main/java/com/samnart/ecommerce/repository/UserRepository.java
@@ -1,17 +1,31 @@
 package com.samnart.ecommerce.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.samnart.ecommerce.model.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {}
 
-    Optional<User> findByUserName(String username);
 
-    Boolean existsByUserName(String username);
 
-    Boolean existsByEmail(String email);
+
+
+// package com.samnart.ecommerce.repository;
+
+// import java.util.Optional;
+
+// import org.springframework.data.jpa.repository.JpaRepository;
+
+// import com.samnart.ecommerce.model.User;
+
+// public interface UserRepository extends JpaRepository<User, Long> {
+
+//     Optional<User> findByUserName(String username);
+
+//     Boolean existsByUserName(String username);
+
+//     Boolean existsByEmail(String email);
     
-}
+// }
