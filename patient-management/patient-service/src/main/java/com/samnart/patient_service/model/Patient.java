3f74cc5fde149fec208ab78ded12f5e53b8f77d7
samnart1 <originalsamnart@gmail.com>
2025-03-17 17:59:29 +0100

    initial commit ~ patient-management system microservice

---
+++ patient-management/patient-service/src/main/java/com/samnart/patient_service/model/Patient.java
@@ -0,0 +1,42 @@
+package com.samnart.patient_service.model;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Patient {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private UUID id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Email
+    @Column(unique=true)
+    private String email;
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    private LocalDate dateOfBirth;
+
+    @NotNull
+    private LocalDate registeredDate;
+}
---
+++ patient-management/patient-service/src/main/java/com/samnart/patient_service/repository/PatientRepository.java
@@ -0,0 +1,13 @@
+package com.samnart.patient_service.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.samnart.patient_service.model.Patient;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, UUID> {
+    
+}
