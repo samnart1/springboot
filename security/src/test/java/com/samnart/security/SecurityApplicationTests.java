c453ff844477277c662aabcdc5bc67ea72968c80
samnart1 <originalsamnart@gmail.com>
2025-07-21 13:48:48 +0200

    new project

---
+++ security/src/test/java/com/samnart/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samnart.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
