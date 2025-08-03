f6c8160d039506e0b23083e2be7e09e14c279b4f
samnart1 <originalsamnart@gmail.com>
2025-08-03 22:57:39 +0200

    chore: figuring out security in springboot still

--- 002security/src/main/java/com/samnart/security/entity/User.java
+++ 002security/src/main/java/com/samnart/security/entity/User.java
@@ -1,14 +1,44 @@
 package com.samnart.entity
 
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User implements UserDetails {
 
   @Id
   @GeneratedValue(stratey = GenerationType.IDENTITY)
   private Long id; 
 
+  private String username;
   
+  private String email;
+
+  private String password;
+
+  private String firstName;
+
+  private String lastName;
+
+  private Role role = Role.User;
+
+  private boolean enabled = true;
+
+  private LocalDateTime createdAt;
+
+  private LocalDateTime updatedAt;
+
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+  }
 }
