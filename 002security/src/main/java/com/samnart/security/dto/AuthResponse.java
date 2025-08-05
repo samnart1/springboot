166ca59d222ef9d45610785f9b401a80ce017bdb
samnart1 <originalsamnart@gmail.com>
2025-08-05 11:34:12 +0200

    chore: add dto and modify user model

---
+++ 002security/src/main/java/com/samnart/security/dto/AuthResponse.java
@@ -0,0 +1,15 @@
+package com.samnart.security.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+    
+    private String token;
+    private String type = "Bearer";
+    private UserResponse user;
+}
--- 002security/src/main/java/com/samnart/security/entity/User.java
+++ 002security/src/main/java/com/samnart/security/entity/User.java
@@ -1,44 +1,95 @@
-package com.samnart.entity
+package com.samnart.security.entity;
 
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 
-import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
-@Table(name = "users")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "users")
+
 public class User implements UserDetails {
 
-  @Id
-  @GeneratedValue(stratey = GenerationType.IDENTITY)
-  private Long id; 
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
   private String username;
-  
   private String email;
-
   private String password;
-
   private String firstName;
-
   private String lastName;
-
-  private Role role = Role.User;
-
+  private Role role = Role.USER;
   private boolean enabled = true;
-
   private LocalDateTime createdAt;
-
   private LocalDateTime updatedAt;
 
+
+
+
+  @Override
   public Collection<? extends GrantedAuthority> getAuthorities() {
     return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+  }
+
+  @Override
+  public String getPassword() {
+    return password;
+  }
+
+  @Override
+  public String getUsername() {
+    return username;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return enabled;
+  }
+
+  @PrePersist
+  protected void onCreate() {
+    createdAt = LocalDateTime.now();
+    updatedAt = LocalDateTime.now();
+  }
+
+  @PreUpdate
+  protected void onUpdate() {
+    updatedAt = LocalDateTime.now();
+  }
+
+  public enum Role {
+    USER,
+    ADMIN
   }
 }
