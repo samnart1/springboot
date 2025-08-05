38df8d47a3ab7e9a310600b1b22906cc2d96e94e
samnart1 <originalsamnart@gmail.com>
2025-08-05 15:11:05 +0200

    chore: add exeptions and user repository

--- 002security/src/main/java/com/samnart/security/repository/UserRepository.java
+++ 002security/src/main/java/com/samnart/security/repository/UserRepository.java
@@ -3,9 +3,16 @@ package com.samnart.security.repository;
 
 import com.samnart.security.entity.User;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {}
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+}
