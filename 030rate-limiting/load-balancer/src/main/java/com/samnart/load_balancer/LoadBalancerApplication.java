83b579f0841e6ae46ab7d51344d2eb06c11f399c
samnart1 <originalsamnart@gmail.com>
2025-09-04 23:19:23 +0200

    chore: asdf

--- 030rate-limiting/load-balancer/src/main/java/com/samnart/load_balancer/LoadBalancerApplication.java
+++ 030rate-limiting/load-balancer/src/main/java/com/samnart/load_balancer/LoadBalancerApplication.java
@@ -2,12 +2,21 @@ package com.samnart.load_balancer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class LoadBalancerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoadBalancerApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public WebClient.Builder loadBalancedWebClientBuilder() {
+		return WebClient.builder();
 	}
 
 }
