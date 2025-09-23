c6dee1caec323095f089cbaa0d022edce0ff3516
samnart1 <originalsamnart@gmail.com>
2025-09-23 07:31:14 +0200

    chore: add pom file and corsWebfilter method

--- 030rate-limiting/api-gateway/src/main/java/com/samnart/api_gateway/ApiGatewayApplication.java
+++ 030rate-limiting/api-gateway/src/main/java/com/samnart/api_gateway/ApiGatewayApplication.java
@@ -2,7 +2,11 @@ package com.samnart.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ApiGatewayApplication {
 
